72b029608defc7d562d097ecf942ab7d3a467294
k vamsi reddy <96013251+vamsireddy1998@users.noreply.github.com>
2022-01-21 11:16:12 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 http://35.154.74.241:9000/const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.154.74.241:9000/',
+  serverUrl: http://35.154.74.241:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'd54a4df99f9993490a6f62cb544cde7b807a3624',
+	    'sonar.login': '683a9daab607acee969269d53fea19fcc01f9adb',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
